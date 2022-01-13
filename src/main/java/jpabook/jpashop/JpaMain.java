32b78eee8ee31c7d9f951dccca9b8546351502cc
ShimKiYoung <shim8934@gmail.com>
2022-01-13 21:28:33 +0900

    JPA 기본 강의 예제 프로젝트 4일차
    소스 변경 +
    
    4일차 - 다양한 연관 관계 실습 코드 push

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * @author holyeye
+ */
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
+        EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
+
+        EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+        tx.begin(); //트랜잭션 시작
+
+        try {
+
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
+
+            tx.commit();//트랜잭션 커밋
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback(); //트랜잭션 롤백
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+
+        emf.close(); //엔티티 매니저 팩토리 종료
+    }
+}
